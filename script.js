5771a6c088f5b87fec4c4e3697ecae6a0f082949
delirium02 <darknightbs@gmail.com>
2025-10-17 02:44:53 +1100

    created grid with working hover functionality.

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container');
+
+for (let i = 0; i < 256; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    container.appendChild(box);
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'black';
+    })
+};
