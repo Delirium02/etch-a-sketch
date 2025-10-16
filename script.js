6265c9fbedc9ad85bed446399cee7a9576553c5b
delirium02 <darknightbs@gmail.com>
2025-10-17 03:18:00 +1100

    created a function for a custom grid size

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const container = document.getElementById('container');
 
-for (let i = 0; i < 256; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'black';
-    })
+const btn = document.createElement('button');
+btn.classList.add('btn')
+btn.textContent = "Custom grid size";
+document.body.insertBefore(btn, container);
+
+function makeGrid(size) {
+    container.innerHTML = '';
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'black';
+        });
+        container.appendChild(box);
+    }
 };
+
+makeGrid(16);
+
+btn.addEventListener("click", () => {
+    let newSize = prompt("Enter column x rows size");
+    newSize = parseInt(newSize);
+
+    if (newSize > 0 && newSize <= 100) {
+        makeGrid(newSize);
+    } else {
+        alert("Please input a number between 1 and 100");
+    }
+    
+});
