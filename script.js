352dea22cea6871af12261e855083c06710a0aac
delirium02 <darknightbs@gmail.com>
2025-10-23 06:52:29 +1100

    reformatted index.html reformatted script.js

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 
-const btn = document.createElement('button');
-btn.classList.add('btn');
+const btn = document.createElement("button");
+btn.classList.add("btn");
 btn.textContent = "Custom grid size";
 document.body.insertBefore(btn, container);
 
-const resetBtn = document.createElement('button');
-resetBtn.classList.add('resetBtn');
+const resetBtn = document.createElement("button");
+resetBtn.classList.add("resetBtn");
 resetBtn.textContent = "Reset grid";
 container.after(resetBtn);
 
 function getRandomColor() {
-    return `${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}`;
-};
+  return `${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)}`;
+}
 
 function makeGrid(size) {
-    container.innerHTML = '';
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.innerHTML = "";
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < size * size; i++) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.dataset.opacity = 0.2;
-        
-        const baseColor = getRandomColor();
-        
-        box.addEventListener('mouseover', () => {
-            let currentOpacity = parseFloat(box.dataset.opacity);
-            
-            if (currentOpacity < 1) {
-                currentOpacity = Math.min(1, currentOpacity += 0.1);
-                box.style.backgroundColor = `rgba(${baseColor}, ${currentOpacity})`;
-                box.dataset.opacity = currentOpacity;
-            }
-        });
-        
-        container.appendChild(box);
-    }
-};
+  for (let i = 0; i < size * size; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.dataset.opacity = 0.2;
+
+    const baseColor = getRandomColor();
+
+    box.addEventListener("mouseover", () => {
+      let currentOpacity = parseFloat(box.dataset.opacity);
+
+      if (currentOpacity < 1) {
+        currentOpacity = Math.min(1, (currentOpacity += 0.1));
+        box.style.backgroundColor = `rgba(${baseColor}, ${currentOpacity})`;
+        box.dataset.opacity = currentOpacity;
+      }
+    });
+
+    container.appendChild(box);
+  }
+}
 
 makeGrid(16);
 
 btn.addEventListener("click", () => {
-    let newSize = parseInt(prompt("Enter column x rows size"));
+  let newSize = parseInt(prompt("Enter column x rows size"));
 
-    if (newSize > 0 && newSize <= 100) {
-        makeGrid(newSize);
-    } else {
-        alert("Please input a number between 1 and 100");
-    }  
+  if (newSize > 0 && newSize <= 100) {
+    makeGrid(newSize);
+  } else {
+    alert("Please input a number between 1 and 100");
+  }
 });
 
 resetBtn.addEventListener("click", () => {
-    makeGrid(16);
+  makeGrid(16);
 });
